624fbf17f2ac5175b17fd9eaf1ae6000a5889115
sorb3nt <sorb3nt@gmail.com>
2022-07-05 16:24:36 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let  result = "";
+    let nStr = Math.abs(n).toString();
+
+    for (let i = nStr.length - 1; i >= 0 ; i-- ) {
+      result += nStr[i] ;
+    }
+
+    return +result;
+  }
